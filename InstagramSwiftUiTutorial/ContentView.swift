b2cb862a632a97ee075827b974a626c7262866e4
badoil <soulish515151@gmail.com>
2021-10-24 21:56:21 +0900

    Initial Commit

---
+++ InstagramSwiftUiTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramSwiftUiTutorial
+//
+//  Created by 장준호 on 2021/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramSwiftUiTutorial/InstagramSwiftUiTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUiTutorialApp.swift
+//  InstagramSwiftUiTutorial
+//
+//  Created by 장준호 on 2021/10/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUiTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
