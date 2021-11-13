bb186905f97cf65d71bf992acb4ecc22c66fb252
badoil <soulish515151@gmail.com>
2021-11-13 17:20:07 +0900

    update: SignupView

---
+++ InstagramSwiftUiTutorial/Utils/RegistrationView.swift
@@ -0,0 +1,119 @@
+//
+//  RegistrationView.swift
+//  InstagramSwiftUiTutorial
+//
+//  Created by 장준호 on 2021/11/13.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    
+    @State private var text = ""
+    @State private var username = ""
+    @State private var fullname = ""
+    @State private var password = ""
+    @State private var selectedImage: UIImage?
+    @State private var image: Image?
+    @State var imagePickerPresented = false
+
+    @Environment(\.presentationMode) var mode
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack {
+                ZStack {
+                    if let image = image {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 150, height: 150)
+                            .clipShape(Circle())
+                            .padding(.top, 44)
+                    } else {
+                        Button(action: {imagePickerPresented.toggle()}, label: {
+                            Image("plus_symbol")
+                                .renderingMode(.template)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 150, height: 150)
+                                .foregroundColor(.white)
+                        }).sheet(isPresented: $imagePickerPresented, onDismiss: loadImage , content: {
+                            ImagePicker(image: $selectedImage)
+                        })
+                    }
+                }
+
+                
+                VStack(spacing: 20) {
+                    CustomTextField(text: $text, placeholder: Text("Email"), imageName: "envelope")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 32)
+                        .foregroundColor(.white)
+                    
+                    CustomTextField(text: $text, placeholder: Text("Username"), imageName: "person")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 32)
+                        .foregroundColor(.white)
+                    
+                    CustomTextField(text: $text, placeholder: Text("Full name"), imageName: "person")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 32)
+                        .foregroundColor(.white)
+                    
+                    CustomSecureField(text: $password, placeholder: Text("Password"))
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 32)
+                        .foregroundColor(.white)
+                }
+                
+                Button(action: {}, label: {
+                    Text("Sign in")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 330, height: 50)
+                        .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
+                        .clipShape(Capsule())
+                        .padding()
+                })
+                
+                Spacer()
+                
+                Button(action: {mode.wrappedValue.dismiss()}, label: {
+                    HStack{
+                        Text("Already have an account??")
+                            .font(.system(size: 14))
+                        Text("Sign in")
+                            .font(.system(size: 14, weight: .semibold))
+                    }.foregroundColor(.white)
+                })
+
+            }
+        }
+    }
+}
+
+extension RegistrationView {
+    func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+        image = Image(uiImage: selectedImage)
+    }
+}
+
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
